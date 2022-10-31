e21bc5da6c9b510e79bf7a2fdf576e583fa6adf3
Wallace Balaniuc <Balaniuc>
2022-10-31 14:03:21 -0400

    Initial commit

---
+++ app/src/main/java/com/mdev/tasks/TaskDiffItemCallback.kt
@@ -0,0 +1,10 @@
+package com.mdev.tasks
+
+import androidx.recyclerview.widget.DiffUtil
+
+class TaskDiffItemCallback : DiffUtil.ItemCallback<Task>() {
+
+    override fun areItemsTheSame(oldItem: Task, newItem: Task) = (oldItem.taskId == newItem.taskId)
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task) = (oldItem == newItem)
+}
